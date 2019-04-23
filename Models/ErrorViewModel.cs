722c3a6334f67d52fab74c2fb3e62562e79b47a5
zhangxi1 <zhangxi1@JTPC-GN1CT92>
2019-04-23 19:30:32 +0800

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace times_groupon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
